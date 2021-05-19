9a201c83e4816a01b81604f3a4522ed271068891
sebastianolmos <sebastianolmos@ug.uchile.cl>
2021-05-19 19:13:16 -0400

    implementacion en CPU con ifs hecha

---
+++ GameOfLifeCPU2/GameOfLifeCPU2/GameOfLifeCPU2.cpp
@@ -0,0 +1,106 @@
+// GameOfLifeCPU2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#include <stdio.h>     
+#include <stdlib.h>
+#include <iostream>
+#include <time.h> 
+#include <chrono>
+
+typedef unsigned char ubyte;
+ubyte* data = NULL;
+ubyte* resultData;
+
+size_t worldHeight;
+size_t worldWidth;
+size_t dataLength;
+
+
+void initBuffers() {
+    data = new ubyte[worldHeight * worldWidth];
+    resultData = new ubyte[worldHeight * worldWidth];
+    dataLength = worldWidth * worldHeight;
+
+    for (size_t i = 0; i < worldHeight; i++) {
+        data[i] = rand() & 1;
+        resultData[i] = 0;
+    }
+}
+
+ubyte countAliveCells(size_t x0, size_t x1, size_t x2, size_t y0, size_t y1, size_t y2) {
+    ubyte result = 0;
+    if (data[x0 + y0] != 0) {
+        result = result + 1;
+    }
+    if (data[x1 + y0] != 0) {
+        result = result + 1;
+    }
+    if (data[x2 + y0] != 0) {
+        result = result + 1;
+    }
+    if (data[x0 + y1] != 0) {
+        result = result + 1;
+    }
+    if (data[x2 + y1] != 0) {
+        result = result + 1;
+    }
+    if (data[x0 + y2] != 0) {
+        result = result + 1;
+    }
+    if (data[x1 + y2] != 0) {
+        result = result + 1;
+    }
+    if (data[x2 + y2] != 0) {
+        result = result + 1;
+    }
+    return result;
+}
+
+void worldIteration() {
+    for (size_t j = 0; j < worldHeight; j++) {
+        size_t y0 = ((j - 1 + worldHeight) % worldHeight) * worldWidth;
+        size_t y1 = j * worldWidth;
+        size_t y2 = ((j + 1) % worldHeight) * worldWidth;
+        for (size_t i = 0; i < worldWidth; i++) {
+            size_t x = i;
+            size_t x0 = (x - 1 + worldWidth) % worldHeight;
+            size_t x2 = (x + 1) % worldWidth;
+
+            ubyte aliveCells = countAliveCells(x0, x, x2, y0, y1, y2);
+            if (aliveCells == 3 || (aliveCells == 2 && data[y1 + x])) {
+                resultData[y1 + x] = 1;
+            }
+            else {
+                resultData[y1 + x] = 0;
+            }
+        }
+    }
+    std::swap(data, resultData);
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 3)
+    {
+        worldWidth = 10000;
+        worldHeight = 10000;
+    }
+    else {
+        worldWidth = atoi(argv[1]);
+        worldHeight = atoi(argv[2]);
+    }
+    /* initialize random seed: */
+    srand(time(NULL));
+
+    // Se inicializan los buffers
+    initBuffers();
+
+    auto start = std::chrono::steady_clock::now();
+    // Se ejecuta una iteracion
+    worldIteration();
+    auto end = std::chrono::steady_clock::now();
+    // nanoseconds
+    // microseconds
+    // milliseconds
+
+    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
+}
---
+++ GameOfLifeCPU2/README.md
@@ -0,0 +1,3 @@
+# Implementacion serial en CPU usando if statements
+
+Genera un programa ejecutable que entrega el tiempo (o) tiempos que demora una iteracion (o iteraciones) del juego de la vida
