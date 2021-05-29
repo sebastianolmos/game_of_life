f00ee9378c75bbfd0b29b56d153fa5d574d64dd4
sebastianolmos <sebastianolmos@ug.uchile.cl>
2021-05-29 18:08:05 -0400

    data agregado para tpb potencias de 3

---
+++ DataGenerator/CUDA_9tpb.py
@@ -0,0 +1,37 @@
+"""Generar data de la implementacion serial en CPU"""
+
+from subprocess import Popen, PIPE
+import numpy as np
+import csv
+
+path = "GameOfLifeCUDA/GameOfLifeCUDA/x64/Release/"
+
+def getValueFromProgram(width, height, iterations, threads) -> float:
+    p = Popen(['GameOfLifeCUDA.exe',  str(width), str(height), str(iterations), str(threads)], cwd=path, shell=True, stdout=PIPE, stdin=PIPE)
+    result = p.stdout.readline().strip()
+    return result.decode()
+
+if __name__ == "__main__":
+
+    width = 27
+    height = 27
+    worlds = 14
+    iterations = 5
+    threads = 9
+
+    with open('Data/cuda_9tpb.csv', mode='w') as csv_file:
+        fieldnames = ['width', 'height', 'iter', 'time']
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+        writer.writeheader()
+
+        for i in range(worlds):
+            if (i % 2):
+                if(i < worlds - 1): 
+                    height *= 3
+                else:
+                    break
+            else:
+                width *= 3
+            time = getValueFromProgram(width, height, iterations, threads)
+            writer.writerow({'width': width, 'height': height, 'iter': iterations, 'time': float(time)})
